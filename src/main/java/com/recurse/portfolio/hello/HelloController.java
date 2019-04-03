3daaafe5eb7fc11a52d957b1697d62be308a6186
Jason Owen <jason@jasonaowen.net>
2019-04-03 13:46:07 -0400

    Add initial Spring Boot application
    Follow the Spring Boot getting started guide[1] to create an initial
    hello world web application.
    
    [1] https://spring.io/guides/gs/spring-boot/
    
    Issue #1 Add initial project structure

---
+++ src/main/java/com/recurse/portfolio/hello/HelloController.java
@@ -0,0 +1,12 @@
+package com.recurse.portfolio.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "Hello, world!";
+    }
+}
