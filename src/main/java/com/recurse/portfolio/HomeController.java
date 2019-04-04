436acdd81950b8aa87a37cba62b484550f0a00ed
Jason Owen <jason@jasonaowen.net>
2019-04-04 17:04:48 -0400

    Add initial home page with Thymeleaf
    Wire up Thymeleaf[1] and create an initial template for the home page.
    
    [1] https://www.thymeleaf.org/

--- src/main/java/com/recurse/portfolio/HomeController.java
+++ src/main/java/com/recurse/portfolio/HomeController.java
@@ -1,12 +1,12 @@
-package com.recurse.portfolio.hello;
+package com.recurse.portfolio;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HelloController {
+@Controller
+public class HomeController {
     @RequestMapping("/")
     public String index() {
-        return "Hello, world!";
+        return "home";
     }
 }
