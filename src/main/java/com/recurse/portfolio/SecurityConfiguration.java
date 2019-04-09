e05af4f9858ff04befcf6ec94ac48565f57c0b4b
Jason Owen <jason@jasonaowen.net>
2019-04-09 17:24:59 -0400

    Remove unused import

--- src/main/java/com/recurse/portfolio/SecurityConfiguration.java
+++ src/main/java/com/recurse/portfolio/SecurityConfiguration.java
@@ -1,7 +1,6 @@
 package com.recurse.portfolio;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
