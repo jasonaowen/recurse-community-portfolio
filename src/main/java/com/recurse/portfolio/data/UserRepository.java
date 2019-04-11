50d8611fedbaa0afe17d23ea913ad85e72bbba0b
Jason Owen <jason@jasonaowen.net>
2019-04-11 13:57:30 -0400

    Move data-related classes into their own package

--- src/main/java/com/recurse/portfolio/data/UserRepository.java
+++ src/main/java/com/recurse/portfolio/data/UserRepository.java
@@ -1,11 +1,11 @@
-package com.recurse.portfolio;
+package com.recurse.portfolio.data;
 
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-interface UserRepository
+public interface UserRepository
     extends CrudRepository<User, Integer>
 {
     @Query("select * from users where recurse_profile_id = :recurseProfileId")
