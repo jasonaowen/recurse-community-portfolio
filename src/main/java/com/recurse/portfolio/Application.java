ed533874097d98d1a4b57ff1b6baadb5c0312d1e
Jason Owen <jason@jasonaowen.net>
2019-04-11 13:53:44 -0400

    Show descriptive error if missing env variables
    When Spring tries to configure itself when some of the environment
    variables we reference in application.yml are missing, the error
    messages can be long and obscure. Create a list of required environment
    variables, check for them before entering Spring, and show a descriptive
    error message if any are missing.

--- src/main/java/com/recurse/portfolio/Application.java
+++ src/main/java/com/recurse/portfolio/Application.java
@@ -2,10 +2,41 @@ package com.recurse.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Application {
+    private static final List<String> REQUIRED_ENVIRONMENT_VARIABLES = List.of(
+        "CLIENT_ID",
+        "CLIENT_SECRET",
+        "JDBC_DATABASE_URL",
+        "JDBC_DATABASE_USERNAME",
+        "JDBC_DATABASE_PASSWORD"
+    );
+
     public static void main(String[] args) {
+        verifyRequiredEnvironmentVariables();
         SpringApplication.run(Application.class, args);
+    }
+
+    private static void verifyRequiredEnvironmentVariables() {
+        List<String> missingVariables = REQUIRED_ENVIRONMENT_VARIABLES.stream()
+            .filter(k -> StringUtils.isEmpty(System.getenv(k)))
+            .collect(Collectors.toList());
+        if (!missingVariables.isEmpty()) {
+            throw new IllegalStateException(
+                "The application could not start because it requires several " +
+                    "environment variables for configuration. " +
+                    "The missing environment variable(s) are: " +
+                    String.join(", ", missingVariables) +
+                    ". Please set these environment variables, " +
+                    "perhaps by sourcing your .env file; " +
+                    "if you already have, update it to match " +
+                    "the .env.template file."
+            );
+        }
     }
 }
