ebb1753cd9a71ec816defd2bb3b96ca4355e54fe
Jason Owen <jason@jasonaowen.net>
2019-04-16 14:29:13 -0400

    Show current user or login link
    Automatically insert the current user (if any) to every model (all one
    of them so far), and use that in the layout to show either a greeting or
    a login link.

--- src/main/java/com/recurse/portfolio/HomeController.java
+++ src/main/java/com/recurse/portfolio/HomeController.java
@@ -1,7 +1,5 @@
 package com.recurse.portfolio;
 
-import com.recurse.portfolio.data.User;
-import com.recurse.portfolio.security.CurrentUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,13 +7,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public ModelAndView index(@CurrentUser User user) {
-        if (user == null) {
-            return new ModelAndView("home");
-        } else {
-            ModelAndView mv = new ModelAndView("secure");
-            mv.addObject("username", user.getInternalName());
-            return mv;
-        }
+    public ModelAndView index() {
+        return new ModelAndView("home");
     }
 }
