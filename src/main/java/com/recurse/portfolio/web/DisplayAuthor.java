09838720dd386e3ee1cc1cb224fa87c4c068a3c1
Jason Owen <jason@jasonaowen.net>
2019-05-03 15:23:47 -0400

    Hide authors marked as private on project page
    Protect the privacy of project authors who are not visible to the
    current user by hiding their name and photo on the project view/edit
    pages; instead, show the placeholder graphic, "Anonymous" as the name,
    and link to a nonexistant user 0.
    
    Issue #46 Do not show author if profile is not visible

---
+++ src/main/java/com/recurse/portfolio/web/DisplayAuthor.java
@@ -0,0 +1,64 @@
+package com.recurse.portfolio.web;
+
+import com.recurse.portfolio.data.User;
+import lombok.NonNull;
+import lombok.Value;
+
+@Value
+public class DisplayAuthor {
+    int userId;
+    String name;
+    String imageUrl;
+
+    public static DisplayAuthor fromUserForUser(
+        @NonNull User sourceUser,
+        User requestingUser
+    ) {
+        switch(sourceUser.getProfileVisibility()) {
+            case PRIVATE:
+                if (sourceUser == requestingUser) {
+                    return useInternal(sourceUser);
+                } else {
+                    return anonymous();
+                }
+            case INTERNAL:
+                if (requestingUser == null) {
+                    return anonymous();
+                } else {
+                    return useInternal(sourceUser);
+                }
+            case PUBLIC:
+                if (requestingUser == null) {
+                    return usePublic(sourceUser);
+                } else {
+                    return useInternal(sourceUser);
+                }
+            default:
+                return anonymous();
+        }
+    }
+
+    private static DisplayAuthor anonymous() {
+        return new DisplayAuthor(
+            0,
+            "Anonymous",
+            "/user-placeholder.png"
+        );
+    }
+
+    private static DisplayAuthor usePublic(User user) {
+        return new DisplayAuthor(
+            user.getUserId(),
+            user.getPublicName(),
+            user.getPublicImageUrl()
+        );
+    }
+
+    private static DisplayAuthor useInternal(User user) {
+        return new DisplayAuthor(
+            user.getUserId(),
+            user.getInternalName(),
+            user.getInternalImageUrl()
+        );
+    }
+}
