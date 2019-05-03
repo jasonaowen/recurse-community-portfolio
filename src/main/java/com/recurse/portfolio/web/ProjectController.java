jasonaowen/recurse-community-portfolio

commit 86e0d2552479c1d1f3439fcdb4925da1d6d43254

Require project and user names
Validate the input sent to the server to make sure that the user's
public and internal names are present and not empty, and to make sure
that the project's name is present and not empty.

Mark the fields as `required`[1] in the HTML so that the browser will
help us out.

[1] https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#attr-required

Issue #42 Validate user input

Co-authored-by: Travis Horton <travis@travish.com>

src/main/java/com/recurse/portfolio/web/ProjectController.java
@@ -10,12 +10,16 @@ import com.recurse.portfolio.security.VisibilityPolicy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 
 import static com.recurse.portfolio.web.MarkdownHelper.renderMarkdownToHtml;
@@ -37,26 +41,35 @@ public class ProjectController {
         project.setVisibility(Visibility.PRIVATE);
         mv.addObject("authors", Set.of(currentUser));
         mv.addObject("project", project);
+        mv.addObject("errors", Collections.emptyList());
         return mv;
     }
 
     @PostMapping("/project/")
     @Transactional
-    public RedirectView postNewProject(
+    public ModelAndView postNewProject(
         @CurrentUser User currentUser,
         Project postedProject
     ) {
         if (currentUser == null) {
             throw new VisibilityException(Visibility.PRIVATE);
         }
-        Project newProject = new Project();
-        updateMutableProjectValues(newProject, postedProject);
-        Project savedProject = repository.save(newProject);
-        repository.addProjectAuthor(
-            savedProject.getProjectId(),
-            currentUser.getUserId()
-        );
-        return new RedirectView("/project/" + savedProject.getProjectId());
+        List<String> errors = validate(postedProject);
+        if (errors.isEmpty()) {
+            Project newProject = new Project();
+            updateMutableProjectValues(newProject, postedProject);
+            Project savedProject = repository.save(newProject);
+            repository.addProjectAuthor(
+                savedProject.getProjectId(),
+                currentUser.getUserId()
+            );
+            return new ModelAndView(new RedirectView("/project/" + savedProject.getProjectId()));
+        } else {
+            return new ModelAndView("projects/new")
+                .addObject("authors", Set.of(currentUser))
+                .addObject("project", postedProject)
+                .addObject("errors", errors);
+        }
     }
 
     @GetMapping("/project/{projectId}")
@@ -110,11 +123,12 @@ public class ProjectController {
         ModelAndView mv = new ModelAndView("projects/edit");
         mv.addObject("project", project);
         mv.addObject("authors", authors);
+        mv.addObject("errors", Collections.emptyList());
         return mv;
     }
 
     @PostMapping("/project/{id}/edit")
-    public RedirectView postEditProject(
+    public ModelAndView postEditProject(
         @CurrentUser User currentUser,
         @PathVariable(name = "id") Integer projectId,
         Project postedProject
@@ -126,10 +140,27 @@ public class ProjectController {
             throw new VisibilityException(Visibility.PRIVATE);
         }
 
-        updateMutableProjectValues(project, postedProject);
-        repository.save(project);
+        List<String> errors = validate(postedProject);
+        if (errors.isEmpty()) {
+            updateMutableProjectValues(project, postedProject);
+            repository.save(project);
 
-        return new RedirectView("/project/" + projectId);
+            return new ModelAndView(new RedirectView("/project/" + projectId));
+        } else {
+            postedProject.setProjectId(projectId);
+            return new ModelAndView("projects/edit")
+                .addObject("project", postedProject)
+                .addObject("errors", errors);
+        }
+    }
+
+    private List<String> validate(Project postedProject) {
+        List<String> errors = new ArrayList<>();
+        if (StringUtils.isEmpty(postedProject.getName())) {
+            errors.add("name");
+        }
+        return errors;
+
     }
 
     private void updateMutableProjectValues(

src/main/java/com/recurse/portfolio/web/UserController.java
@@ -10,12 +10,15 @@ import com.recurse.portfolio.security.VisibilityPolicy;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 
@@ -103,11 +106,12 @@ public class UserController {
         );
 
         mv.addObject("user", requestedUser);
+        mv.addObject("errors", Collections.emptyList());
         return mv;
     }
 
     @PostMapping("/user/{id}/edit")
-    public RedirectView postEditMyProfile(
+    public ModelAndView postEditMyProfile(
         @CurrentUser User currentUser,
         @PathVariable(name = "id") Integer userId,
         User postedUser
@@ -118,10 +122,29 @@ public class UserController {
             throw new VisibilityException(Visibility.PRIVATE);
         }
 
-        updateMutableCurrentUserValues(currentUser, postedUser);
-        repository.save(currentUser);
+        List<String> errors = validate(postedUser);
+        if (errors.isEmpty()) {
+            updateMutableCurrentUserValues(currentUser, postedUser);
+            repository.save(currentUser);
 
-        return new RedirectView("/user/" + userId);
+            return new ModelAndView(new RedirectView("/user/" + userId));
+        } else {
+            postedUser.setUserId(currentUser.getUserId());
+            return new ModelAndView("users/edit")
+                .addObject("user", postedUser)
+                .addObject("errors", errors);
+        }
+    }
+
+    private List<String> validate(User postedUser) {
+        List<String> errors = new ArrayList<>();
+        if (StringUtils.isEmpty(postedUser.getPublicName())) {
+            errors.add("publicName");
+        }
+        if (StringUtils.isEmpty(postedUser.getInternalName())) {
+            errors.add("internalName");
+        }
+        return errors;
     }
 
     private void updateMutableCurrentUserValues(

CONTRIBUTORS.markdown
@@ -20,3 +20,4 @@ Alphabetically by first name:
 - [Ian Ross](https://github.com/ihross)
 - [Sebastian Morr](https://github.com/blinry)
 - [Sumana Harihareswara](https://github.com/brainwane)
+- [Travis Horton](https://www.travish.com/)

src/main/resources/static/style.css
@@ -158,3 +158,7 @@ footer {
   width: 24em;
   height: 32em;
 }
+
+.input-error {
+  color: red;
+}

src/main/resources/templates/projects/edit.html
@@ -13,13 +13,17 @@
   >
     <section class="project-input">
       <section class="project-input-name">
-        <label class="project-input-name-label">
+        <label
+          class="project-input-name-label"
+          data-th-class="'project-input-name-label' + (${#arrays.contains(errors, 'name')}? ' input-error' : '')"
+        >
           Project Name
           <input
             class="project-input-name-input"
             type="text"
             name="name"
             placeholder="My Awesome Project"
+            required
             data-th-value="*{name}"
           />
         </label>

src/main/resources/templates/projects/new.html
@@ -13,13 +13,17 @@
   >
     <section class="project-input">
       <section class="project-input-name">
-        <label class="project-input-name-label">
+        <label
+          class="project-input-name-label"
+          data-th-class="'project-input-name-label' + (${#arrays.contains(errors, 'name')}? ' input-error' : '')"
+        >
           Project Name
           <input
             class="project-input-name-input"
             type="text"
             name="name"
             placeholder="My Awesome Project"
+            required
             data-th-value="*{name}"
           />
         </label>

src/main/resources/templates/users/edit.html
@@ -14,12 +14,15 @@
     <section class="user-details">
       <section class="user-view">
         <h2 class="user-section-title">Internal</h2>
-        <label>
+        <label
+          data-th-class="${#arrays.contains(errors, 'internalName')}? 'input-error'"
+        >
           My name (internal):
           <input
             class="user-input-name"
             type="text"
             name="internalName"
+            required
             data-th-value="*{internalName}"
           />
         </label>
@@ -55,12 +58,15 @@ mollit anim id est laborum.
       </section>
       <section class="user-view">
         <h2 class="user-section-title">Public</h2>
-        <label>
+        <label
+          data-th-class="${#arrays.contains(errors, 'publicName')}? 'input-error'"
+        >
           My name (public):
           <input
             class="user-input-name"
             type="text"
             name="publicName"
+            required
             data-th-value="*{publicName}"
           />
         </label>

commit 9422c34c70932d3728fe8d8ab5fdf8a2b1425964

Remove unused errors model attribute
The `errors` attribute was introduced in commit
86e0d2552479c1d1f3439fcdb4925da1d6d43254 as a list of errors on the POST
model. In order to allow the templates to check for errors without a
null pointer exception, we also had to add an empty list on the GET
models. That empty list is no longer required, and should have been
included in the refactoring in 209e7d642601fa6b5ba330ee88d41a9d94b0dafa.
Remove the unused empty lists.

Issue #42 Validate user input

src/main/java/com/recurse/portfolio/web/ProjectController.java
@@ -18,7 +18,6 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.validation.Valid;
-import java.util.Collections;
 import java.util.Set;
 
 import static com.recurse.portfolio.web.MarkdownHelper.renderMarkdownToHtml;
@@ -40,7 +39,6 @@ public class ProjectController {
         project.setVisibility(Visibility.PRIVATE);
         mv.addObject("authors", Set.of(currentUser));
         mv.addObject("project", project);
-        mv.addObject("errors", Collections.emptyList());
         return mv;
     }
 
@@ -121,7 +119,6 @@ public class ProjectController {
         ModelAndView mv = new ModelAndView("projects/edit");
         mv.addObject("project", project);
         mv.addObject("authors", authors);
-        mv.addObject("errors", Collections.emptyList());
         return mv;
     }
 

src/main/java/com/recurse/portfolio/web/UserController.java
@@ -18,7 +18,6 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.validation.Valid;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 
@@ -106,7 +105,6 @@ public class UserController {
         );
 
         mv.addObject("user", requestedUser);
-        mv.addObject("errors", Collections.emptyList());
         return mv;
     }
 