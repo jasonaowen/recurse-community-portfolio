jasonaowen/recurse-community-portfolio

commit 5bdb236648ddac7c18ad27e5cd9935be07c4cba1

Show recently created projects on home page
Show a list of the 20 most recently created projects (by project ID) on
the home page, along with their authors.

No styling yet; for now, just add the functionality.

Issue #32 Browse projects

src/main/java/com/recurse/portfolio/data/ProjectRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Set;
 
 public interface ProjectRepository
@@ -22,4 +23,57 @@ public interface ProjectRepository
         "values (:projectId, :authorId) " +
         "on conflict(author_id, project_id) do nothing")
     void addProjectAuthor(int projectId, int authorId);
+
+    @Query("select" +
+        "  p.project_id as project_project_id," +
+        "  p.visibility as project_visibility," +
+        "  p.name as project_name," +
+        "  p.description_private as project_description_private," +
+        "  p.description_internal as project_description_internal," +
+        "  p.description_public as project_description_public," +
+        "  a.user_id as author_user_id," +
+        "  a.recurse_profile_id as author_recurse_profile_id," +
+        "  a.name_internal as author_name_internal," +
+        "  a.profile_visibility as author_profile_visibility," +
+        "  a.name_public as author_name_public," +
+        "  a.image_url_internal as author_image_url_internal," +
+        "  a.image_url_public as author_image_url_public," +
+        "  a.bio_internal as author_bio_internal," +
+        "  a.bio_public as author_bio_public " +
+        "from projects p" +
+        "  inner join project_authors pa" +
+        "    on p.project_id = pa.project_id" +
+        "  inner join users a" +
+        "    on pa.author_id = a.user_id " +
+        "where visibility = 'PUBLIC' " +
+        "order by p.project_id desc " +
+        "limit :limit")
+    List<ProjectAndAuthor> getPublicProjects(int limit);
+
+    @Query("select" +
+        "  p.project_id as project_project_id," +
+        "  p.visibility as project_visibility," +
+        "  p.name as project_name," +
+        "  p.description_private as project_description_private," +
+        "  p.description_internal as project_description_internal," +
+        "  p.description_public as project_description_public," +
+        "  a.user_id as author_user_id," +
+        "  a.recurse_profile_id as author_recurse_profile_id," +
+        "  a.name_internal as author_name_internal," +
+        "  a.profile_visibility as author_profile_visibility," +
+        "  a.name_public as author_name_public," +
+        "  a.image_url_internal as author_image_url_internal," +
+        "  a.image_url_public as author_image_url_public," +
+        "  a.bio_internal as author_bio_internal," +
+        "  a.bio_public as author_bio_public " +
+        "from projects p" +
+        "  inner join project_authors pa" +
+        "    on p.project_id = pa.project_id" +
+        "  inner join users a" +
+        "    on pa.author_id = a.user_id " +
+        "where p.visibility in ('PUBLIC', 'INTERNAL')" +
+        "  OR (pa.author_id = :userId) " +
+        "order by p.project_id desc " +
+        "limit :limit")
+    List<ProjectAndAuthor> getProjectsVisibleToUser(int userId, int limit);
 }

src/main/java/com/recurse/portfolio/data/Project.java
@@ -4,8 +4,12 @@ import com.recurse.portfolio.security.Visibility;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
@@ -15,6 +19,9 @@ public class Project {
     Integer projectId;
 
     Visibility visibility;
+
+    @Transient
+    Set<User> authors = new HashSet<>();
 
     String name;
 

src/main/java/com/recurse/portfolio/data/ProjectAndAuthor.java
@@ -1,0 +1,31 @@
+package com.recurse.portfolio.data;
+
+import lombok.Data;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@Table("project_authors")
+public class ProjectAndAuthor {
+    @Column(value = "project_id", keyColumn = "project_id")
+    Project project;
+
+    @Column(value = "author_id", keyColumn = "user_id")
+    User author;
+
+    public static Collection<Project> collect(List<ProjectAndAuthor> list) {
+        Map<Integer, Project> projectsById = new HashMap<>();
+        for (ProjectAndAuthor pa : list) {
+            projectsById.putIfAbsent(pa.project.getProjectId(), pa.project);
+            projectsById.get(pa.project.getProjectId()).getAuthors().add(
+                pa.getAuthor()
+            );
+        }
+        return projectsById.values();
+    }
+}

src/main/java/com/recurse/portfolio/web/HomeController.java
@@ -1,13 +1,40 @@
 package com.recurse.portfolio.web;
 
+import com.recurse.portfolio.data.Project;
+import com.recurse.portfolio.data.ProjectAndAuthor;
+import com.recurse.portfolio.data.ProjectRepository;
+import com.recurse.portfolio.data.User;
+import com.recurse.portfolio.security.CurrentUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Collection;
+
 @Controller
 public class HomeController {
+    private static final int LIMIT = 20;
+
+    @Autowired
+    ProjectRepository projectRepository;
+
     @GetMapping("/")
-    public ModelAndView index() {
-        return new ModelAndView("home");
+    public ModelAndView index(@CurrentUser User currentUser) {
+        return new ModelAndView("home")
+            .addObject("projects", projectsForUser(currentUser));
+    }
+
+    private Collection<Project> projectsForUser(User currentUser) {
+        if (currentUser == null) {
+            var projectAuthors = projectRepository.getPublicProjects(LIMIT);
+            return ProjectAndAuthor.collect(projectAuthors);
+        } else {
+            var projectAuthors = projectRepository.getProjectsVisibleToUser(
+                currentUser.getUserId(),
+                LIMIT
+            );
+            return ProjectAndAuthor.collect(projectAuthors);
+        }
     }
 }

src/main/resources/templates/home.html
@@ -5,7 +5,39 @@
 </head>
 <body>
 <main>
-  <p>Content will go here.</p>
+  <h1>Recent Projects</h1>
+  <ul class="global-project-list">
+    <li
+      class="global-project-list-item"
+      data-th-each="project : ${projects}"
+    >
+      <a
+        href="projects/public.html"
+        data-th-href="@{/project/{id}(id=${project.projectId})}"
+        data-th-text="${project.name}"
+      >Project Name</a>,
+      by
+      <span
+        class="global-project-list-item-author"
+        data-th-each="author : ${project.authors}"
+      >
+        <a
+          href="users/public.html"
+          data-th-href="@{/user/{id}(id=${author.userId})}"
+          data-th-text="${author.publicName}"
+        >Author Name</a><span
+        data-th-if="not ${authorStat.last}"
+      >, </span>
+      </span>
+    </li>
+    <li
+      class="global-project-list-item"
+      data-th-remove="all"
+    >
+      <a href="../projects/public.html">Second Project</a>,
+      by <a href="users/public.html">Second Author</a>
+    </li>
+  </ul>
 </main>
 </body>
 </html>

commit e4cb7ba81bc8040fd30d9e2adf9eacb14f6412c7

Get the correct number of multi-author projects
Fix a lurking not-yet-bug introduced in commit
5bdb236648ddac7c18ad27e5cd9935be07c4cba1 where projects with multiple
authors would cause fewer projects to be fetched. In the process,
extract a shared SQL string for the common bits of the query.

This is relevant now because adding the ability to browse projects by
tag depends on browsing projects with pagination, and that'll in turn
require updating these queries.

Issue #5 Add tags to projects
Issue #32 Browse projects

src/main/java/com/recurse/portfolio/data/ProjectRepository.java
@@ -43,7 +43,7 @@ public interface ProjectRepository
         "  and tag_id = :tagId")
     void removeProjectTag(int projectId, int tagId);
 
-    @Query("select" +
+    String SELECT = "select" +
         "  p.project_id as project_project_id," +
         "  p.visibility as project_visibility," +
         "  p.name as project_name," +
@@ -59,40 +59,33 @@ public interface ProjectRepository
         "  a.image_url_public as author_image_url_public," +
         "  a.bio_internal as author_bio_internal," +
         "  a.bio_public as author_bio_public " +
-        "from projects p" +
+        "from visible_projects v" +
+        "  inner join projects p" +
+        "    on v.project_id = p.project_id" +
         "  inner join project_authors pa" +
         "    on p.project_id = pa.project_id" +
         "  inner join users a" +
         "    on pa.author_id = a.user_id " +
-        "where visibility = 'PUBLIC' " +
-        "order by p.project_id desc " +
-        "limit :limit")
+        "order by p.project_id desc";
+
+    @Query("with visible_projects as (" +
+        "  select * from projects" +
+        "  where visibility = 'PUBLIC'" +
+        "  order by project_id desc" +
+        "  limit :limit" +
+        ") " + SELECT)
     List<ProjectAndAuthor> getPublicProjects(int limit);
 
-    @Query("select" +
-        "  p.project_id as project_project_id," +
-        "  p.visibility as project_visibility," +
-        "  p.name as project_name," +
-        "  p.description_private as project_description_private," +
-        "  p.description_internal as project_description_internal," +
-        "  p.description_public as project_description_public," +
-        "  a.user_id as author_user_id," +
-        "  a.recurse_profile_id as author_recurse_profile_id," +
-        "  a.name_internal as author_name_internal," +
-        "  a.profile_visibility as author_profile_visibility," +
-        "  a.name_public as author_name_public," +
-        "  a.image_url_internal as author_image_url_internal," +
-        "  a.image_url_public as author_image_url_public," +
-        "  a.bio_internal as author_bio_internal," +
-        "  a.bio_public as author_bio_public " +
-        "from projects p" +
-        "  inner join project_authors pa" +
-        "    on p.project_id = pa.project_id" +
-        "  inner join users a" +
-        "    on pa.author_id = a.user_id " +
-        "where p.visibility in ('PUBLIC', 'INTERNAL')" +
-        "  OR (pa.author_id = :userId) " +
-        "order by p.project_id desc " +
-        "limit :limit")
+    @Query("with visible_projects as (" +
+        "  select p.project_id" +
+        "  from projects p" +
+        "    inner join project_authors pa" +
+        "      on p.project_id = pa.project_id" +
+        "  where p.visibility in ('PUBLIC', 'INTERNAL')" +
+        "    OR (pa.author_id = :userId)" +
+        "  group by 1" +
+        "  order by p.project_id desc " +
+        "  limit :limit" +
+        ") " + SELECT)
     List<ProjectAndAuthor> getProjectsVisibleToUser(int userId, int limit);
 }
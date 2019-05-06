e4cb7ba81bc8040fd30d9e2adf9eacb14f6412c7
Jason Owen <jason@jasonaowen.net>
2019-05-06 17:31:11 -0400

    Get the correct number of multi-author projects
    Fix a lurking not-yet-bug introduced in commit
    5bdb236648ddac7c18ad27e5cd9935be07c4cba1 where projects with multiple
    authors would cause fewer projects to be fetched. In the process,
    extract a shared SQL string for the common bits of the query.
    
    This is relevant now because adding the ability to browse projects by
    tag depends on browsing projects with pagination, and that'll in turn
    require updating these queries.
    
    Issue #5 Add tags to projects
    Issue #32 Browse projects

--- src/main/java/com/recurse/portfolio/data/ProjectRepository.java
+++ src/main/java/com/recurse/portfolio/data/ProjectRepository.java
@@ -43,7 +43,7 @@ public interface ProjectRepository
         "  and tag_id = :tagId")
     void removeProjectTag(int projectId, int tagId);
 
-    @Query("select" +
+    String SELECT = "select" +
         "  p.project_id as project_project_id," +
         "  p.visibility as project_visibility," +
         "  p.name as project_name," +
@@ -59,40 +59,33 @@ public interface ProjectRepository
         "  a.image_url_public as author_image_url_public," +
         "  a.bio_internal as author_bio_internal," +
         "  a.bio_public as author_bio_public " +
-        "from projects p" +
+        "from visible_projects v" +
+        "  inner join projects p" +
+        "    on v.project_id = p.project_id" +
         "  inner join project_authors pa" +
         "    on p.project_id = pa.project_id" +
         "  inner join users a" +
         "    on pa.author_id = a.user_id " +
-        "where visibility = 'PUBLIC' " +
-        "order by p.project_id desc " +
-        "limit :limit")
+        "order by p.project_id desc";
+
+    @Query("with visible_projects as (" +
+        "  select * from projects" +
+        "  where visibility = 'PUBLIC'" +
+        "  order by project_id desc" +
+        "  limit :limit" +
+        ") " + SELECT)
     List<ProjectAndAuthor> getPublicProjects(int limit);
 
-    @Query("select" +
-        "  p.project_id as project_project_id," +
-        "  p.visibility as project_visibility," +
-        "  p.name as project_name," +
-        "  p.description_private as project_description_private," +
-        "  p.description_internal as project_description_internal," +
-        "  p.description_public as project_description_public," +
-        "  a.user_id as author_user_id," +
-        "  a.recurse_profile_id as author_recurse_profile_id," +
-        "  a.name_internal as author_name_internal," +
-        "  a.profile_visibility as author_profile_visibility," +
-        "  a.name_public as author_name_public," +
-        "  a.image_url_internal as author_image_url_internal," +
-        "  a.image_url_public as author_image_url_public," +
-        "  a.bio_internal as author_bio_internal," +
-        "  a.bio_public as author_bio_public " +
-        "from projects p" +
-        "  inner join project_authors pa" +
-        "    on p.project_id = pa.project_id" +
-        "  inner join users a" +
-        "    on pa.author_id = a.user_id " +
-        "where p.visibility in ('PUBLIC', 'INTERNAL')" +
-        "  OR (pa.author_id = :userId) " +
-        "order by p.project_id desc " +
-        "limit :limit")
+    @Query("with visible_projects as (" +
+        "  select p.project_id" +
+        "  from projects p" +
+        "    inner join project_authors pa" +
+        "      on p.project_id = pa.project_id" +
+        "  where p.visibility in ('PUBLIC', 'INTERNAL')" +
+        "    OR (pa.author_id = :userId)" +
+        "  group by 1" +
+        "  order by p.project_id desc " +
+        "  limit :limit" +
+        ") " + SELECT)
     List<ProjectAndAuthor> getProjectsVisibleToUser(int userId, int limit);
 }
